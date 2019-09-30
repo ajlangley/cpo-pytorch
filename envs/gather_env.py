Found bug in envs/gather_env.py where I was unpacking the values in the named object tuples in the wrong order, essentially making the task unlearnable.

envs/gather_env.py
@@ -150,7 +150,7 @@ class GatherEnv:
         standardize_radians = lambda x: (x + 2 * pi) % (2 * pi)
         orientation = standardize_radians(orientation)
 
-        for obj_x, obj_y, obj_type in sorted_objs:
+        for obj_type, obj_x, obj_y in sorted_objs:
             dist = euclidian_dist(obj_x, obj_y, agent_x, agent_y)
 
             if dist > self.sensor_range:
@@ -162,10 +162,7 @@ class GatherEnv:
 
             # Get the angle of the vector relative to the agent's sensor range
             angle = np.arctan2(y_comp, x_comp)
-            angle = standardize_radians(angle) - orientation
-
-            angle = np.arctan2(obj_y - agent_y, obj_x - agent_x) - orientation
-            angle = angle % (2 * np.pi)
+            angle = standardize_radians(angle - orientation)
 
             # Skip if object is outside sensor span
             if angle > half_span and angle < 2 * pi - half_span:

envs/point_gather.py
@@ -4,7 +4,7 @@ from envs.gather_env import GatherEnv
 
 
 class PointGatherEnv(GatherEnv):
-    def __init__(self, n_apples=8, n_bombs=8, apple_reward=1, bomb_cost=1,
+    def __init__(self, n_apples=8, n_bombs=8, apple_reward=10, bomb_cost=1,
                  activity_range=6.0, catch_range=1.0, n_bins=10,
                  robot_object_spacing=2.0, sensor_range=6.0, sensor_span=np.pi):
         GatherEnv.__init__(self, 'envs/assets/point.xml', n_apples, n_bombs,
@@ -16,8 +16,8 @@ class PointGatherEnv(GatherEnv):
         self._do_simulation(action)
         n_apples, n_bombs = self._update_objects()
 
-        reward = n_apples * self.apple_reward - self._unhealthy_cost()
         cost = n_bombs * self.bomb_cost
+        reward = n_apples * self.apple_reward - cost
         info = dict(reward=reward, constraint_cost=cost)
 
         self._step_num += 1
@@ -51,6 +51,3 @@ class PointGatherEnv(GatherEnv):
 
     def _is_done(self):
         return self._max_episode_steps and self._step_num >= self._max_episode_steps
-
-    def _unhealthy_cost(self):
-        return 0.0